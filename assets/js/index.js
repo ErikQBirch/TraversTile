00688254921352f2a94da3ad087e2cc1eeb48c24
erikqb3 <erikqb3@gmail.com>
2023-03-30 15:44:08 -0600

    recent hanges

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,8 +9,8 @@ const pageStuff = {
     //   this.footer()
     );
     navigation.postConstructionFunctions();
+    this.favicon();
     // this.scrollEvents.headerScroll();
-    // this.favicon();
     // helperFunctions.metaInfo({
     //   "keywordList":"Portfolio, Web Design, Content Creation, Social Media",
     //   "description":"I am a web-designer and promoter of amazing, wholesome, and uplifting artwork of all kinds. I live in a world that balances between the intracate power of science and the inspiring wonder of art. As a web designer, I excel in creation and beautification when it comes to websites and always seek oportuanities to help others with my skills. Whether it's web designing in and of itself, content copywriting, or obtaining a pressence on social media; this is what I do and have been doing for many years."
@@ -42,15 +42,12 @@ const pageStuff = {
     favicon = document.querySelector("link[rel~='icon']"),
     pathAdjuster = helperFunctions.getPathAdjuster(navigation.pageList)
   ){ 
-    // console.log("favicon",favicon);
-    // console.log(pathAdjuster);
     if (!favicon) {
         favicon = document.createElement('link');
         favicon.rel = 'icon';
         document.getElementsByTagName('head')[0].appendChild(favicon);
     }
-    favicon.href = `${pathAdjuster[0]}assets/resources/imgs/TripleA.ico`;
-    // console.log(favicon);
+    favicon.href = `${pathAdjuster[0]}assets/resources/imgs/TT_icon.ico`;
   },
   footer: function(
     footer_tag = helperFunctions.generateElement('footer'),
